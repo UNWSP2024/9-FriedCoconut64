10fbefd753039f55bad41382b8bfb8ce7d7a311d
FriedCoconut64 <vansterapples@gmail.com>
2025-03-26 20:02:57 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,16 +1,27 @@
-# Program #1: Item Counter
-# Assume a file containing a series of names (as strings) is named names.txt 
-# (Use the included example file names.txt) and exists on the computer's disk.
-# Write a program that displays the number of names that are stored in the file.
+import os.path
+#I found the os import online when looking for solution for the file path in line 12
+import random
+
 
 def count_file_lines():
-    ######################
-    # Add your code here #
-    ######################
-    print('In the count_file_lines function')
+    names = 'John', 'Lucas', 'Adam', 'Stephanie', 'Grace', 'Troy', 'Clare'
+    names_file = open('names.txt', 'w')
+    names_file.write(', '.join(names) + '\n')
+    names_file.close()
+    try:
+        with open(os.path.abspath('names.txt'), 'r') as file:
+            name_count = 0
+            for line in file:
+                name_count = len(line.split(', '))
+        print('In the count_file_lines function,', name_count, 'names were found')
+    except IOError:
+        print("An error occurred trying to read the file", names_file)
+    except ValueError:
+        print('There was an error with the amount of names in', names_file)
 
 
 
-# You don't need to change anything below this line:
+
 if __name__ == '__main__':
     count_file_lines()
+# Program #1, Donovan Thompson 3/26/2025
